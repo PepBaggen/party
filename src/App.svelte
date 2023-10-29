1ba459e76f8936a34162b3489ba04c47c6ea49b8
PepBaggen <pepijnbaggen@gmail.com>
2023-10-29 16:02:57 +0100

    Your descriptive commit message here

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,298 @@
-<script>
-	export let name;
-</script>
-
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<head>
+    <meta charset="UTF-8">
+    <title>PHD-Danielle</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+</head>    
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat&family=Playfair+Display:wght@500&display=swap');
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+ 
+        * {
+    margin: 0;
+    padding: 0;
+}
+        
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+        body {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    width: 100vw;
+    background-color: transparent;
+    background-image: url('https://drive.google.com/uc?id=1nGkdxMEhIPo0XrFn1gMBivmcyq68vD2e');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center center;
+    color: #f5f5f5; /* Creme white text color */
+    font-family: 'Montserrat', sans-serif;
+}
+
+
+
+        .all {
+    text-align: center;
+    font-size: 4vw;
+    padding-top: 3vh; /* Adjust the padding-top value */
+    padding: 5vw 5vw;
+}
+
+.kaart {
+    text-align: center;
+    margin: 0 auto;
+    padding-top: 2vh; /* Adjust the padding-top value */
+}
+
+.all h4 {
+    font-size: 1.5vw;
+    padding-top: 1vh; /* Adjust the padding-top value */
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+    font-weight: normal; /* Remove bold formatting */
+}
+
+
+
+
+        .all h2 {
+            font-size: 5vw; /* Adjust the font size as needed */
+        }
+
+        .all p {
+            padding-top: 10vh;
+            font-size: 4vw; /* Adjust the font size as needed */
+            /* Style the text with a gold color */
+            color: white;
+            /* Add a subtle text-shadow for the glow effect */
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+        }
+
+       
+     
+       
+         .section span a{
+            font-size: 4vw;
+            padding-bottom: 15vh;
+            transition: color 0.5s ease;
+           
+
+         }
+         .section a:hover{
+            color: #66D9EF;
+
+         }
+    
+        .contact-icons {
+            display: flex;
+            font-size: 1vw; /* Adjust the font size as needed */
+            justify-content: center;
+            padding-top: 2vh; /* Vertical centering (adjust value as needed) */
+
+        }
+
+        .contact-icons a {
+            display: inline-block;
+            margin: 0 20px;
+            color: white;
+            transition: color 0.5s ease;
+        }
+
+        .contact-icons a:hover {
+            color: #66D9EF; /* Change color on hover (you can adjust the color) */
+        }
+
+.top {
+    text-align: center;
+    font-size: 1vw;
+    position: sticky;
+    
+}
+.top a{
+    color: white;
+    text-decoration: none;
+    transition: color 0.5s ease;
+
+
+} 
+.top a:hover{
+    color: #66D9EF;
+}       
+
+.payment-icon {
+    display: inline-block;
+}
+
+.coin {
+    width: 25px;
+    height: 25px;
+    border-radius: 50%;
+    background-color: white;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2); /* Optional: Add a subtle shadow effect */
+    transition: background-color 0.3s ease;
+    margin-top: 15px;
+}
+
+.coin:hover {
+    background-color: #66D9EF; /* Change to blue on hover */
+}
+
+
+
+
+
+@media screen and (min-width: 300px) {
+            .all h2 {
+                font-size: 4vw; /* Adjust the font size as needed */
+                padding-top: 5vh;
+            }
+
+            .all p {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            
+
+            .contact-icons {
+                font-size: 4vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                padding-top: 5vh;
+                font-size: 2.5vw;
+            }
+            .top{
+                font-size: 1.5vw;
+            }
+
+        }
+
+
+
+
+
+
+
+@media screen and (min-width: 400px) {
+            .all h2 {
+                font-size: 4vw; /* Adjust the font size as needed */
+                padding-top: 5vh;
+            }
+
+            .all p {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            
+
+            .contact-icons {
+                font-size: 4vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                padding-top: 5vh;
+                font-size: 2.5vw;
+            }
+            .top{
+                font-size: 1.5vw;
+            }
+
+        }
+
+
+
+        @media screen and (min-width: 600px) {
+            .all h2 {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            .all p {
+                font-size: 2vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+                padding-top: 5vh;
+                font-size: 2vw;
+            }
+
+            
+
+            .contact-icons {
+                font-size: 2vw; /* Adjust the font size as needed */
+            }
+            .top{
+                font-size: 1.5vw;
+            }
+        }
+
+        @media screen and (min-width: 1200px) {
+            .all h2 {
+                font-size: 3vw; /* Adjust the font size as needed */
+            }
+
+            .section span{
+            font-size: 2vw;
+         }
+
+            .all p {
+                font-size: 1.5vw; /* Adjust the font size as needed */
+            }
+
+            .contact-icons {
+                font-size: 1.6vw; /* Adjust the font size as needed */
+            }
+            .all h4{
+            font-size: 1vw; /* Adjust the font size as needed */
+            padding-top: 3vh;
+
+
+        }
+        .top{
+                font-size: 1vw;
+            }
+
+
+        }
 </style>
+
+
+
+
+<html lang="en">
+<body>
+    
+    <div class="all">
+        <div class="top"><a href="https://docs.google.com/viewer?url=https://drive.google.com/uc?id=1ycdMVBYuo4p8IVZi078EtwgAuSe4wJtH&embedded=true" target="_blank"><h1>Thesis Link</h1></a></div>
+        <div class="section" id="home">
+            <h2>PhD ceremonie Daniëlle</h2>
+            <h4>Op dinsdag 31 oktober om 16:15 zal Daniëlle haar proefschrift verdedigen aan de Universiteit Leiden. Op deze pagina vindt je tevens ook haar thesis. 
+            <br><br> Als je haar een cadeau wilt geven, zij wilt graag een antieke stationsbank voor in de tuin. Een kleine bijdrage is daarvoor heel welkom. Een bijdrage kan met het ronde icoontje hieronder.
+            <br>Namens Pepijn en Mieke (paranimfen)
+        </h4>
+            <a href="https://tikkie.me/pay/jbh1ffbe3a5uikr2o8bm" target="_blank">
+                <div class="payment-icon">
+                    <a href="https://tikkie.me/pay/jbh1ffbe3a5uikr2o8bm" target="_blank">
+                        <div class="coin"></div>
+                    </a>
+                </div>   </a>
+
+            <p>31 oktober 2023
+                <br><br>Academie Gebouw
+            <br>Rapenburg 73, 2311 GJ Leiden</p>
+            
+        </div>
+        <div class="kaart">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d156656.71680113432!2d4.196923686718741!3d52.15685240000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c5c6f2492d2c39%3A0xa86e57c2a94815d9!2sAcademiegebouw%20Leiden!5e0!3m2!1snl!2snl!4v1696793580081!5m2!1snl!2snl" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        </div>
+        <div class="section" id="contact">
+            <div class="contact-icons">
+                <a href="mailto:davr@me.com" target="_blank">
+                    <i class="fas fa-envelope"></i>
+                </a>
+                <a href="" target="_blank">
+                    <i class="fas fa-phone"></i>
+                    +31622141471
+                </a>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
+
